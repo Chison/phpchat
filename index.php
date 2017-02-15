9c50b13f2d10f3ed0e22dd935d85042c2c7b8ffc
Chison <1335703540@qq.com>
2017-02-15 16:45:04 +0800

    修改注释内容

--- index.php
+++ index.php
@@ -5,11 +5,11 @@ use Ratchet\WebSocket\WsServer;
 use MyApp\Chat;
 
 /**
- * 官方地址：http://socketo.me/docs/hello-world
+ * （源）more: http://socketo.me/docs/hello-world
  * 使用：
  * 1、安装依赖：composer install
  * 2、运行服务端： php index.php
- * 3、测试，打开多个浏览器，查看console。
+ * 3、测试：打开多个浏览器，查看console。
  * */
 
 require __DIR__ . '/vendor/autoload.php';
